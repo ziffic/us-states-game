d08184b2efb427a5109840b8c09148c05d4b54b5
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-15 10:57:46 -0700

    added states to learn csv at the end of game

--- main.py
+++ main.py
@@ -12,18 +12,24 @@ data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
 
 game = Map()
-correct_guesses = []
+guesses_states = []
 current_score = 0
 title_text = "Guess the State"
 
 while current_score < 51:
     guessed_answer = screen.textinput(title=title_text, prompt="What's another state's name?").title()
     if guessed_answer == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guesses_states:
+                missing_states.append(state)
+            new_data = pandas.DataFrame(missing_states)
+            new_data.to_csv("states_to_learn.csv")
         break
 
     if guessed_answer in all_states:
-        if correct_guesses.count(guessed_answer) == 0:
-            correct_guesses.append(guessed_answer)
+        if guesses_states.count(guessed_answer) == 0:
+            guesses_states.append(guessed_answer)
             state_data = data[data.state == guessed_answer]
             game.state = guessed_answer
             game.x = int(state_data.x)
